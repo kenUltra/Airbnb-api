3caecd105f8327469b890774d16cf4ae8a4453d8
Ken Mikaelson <mikaelsoncedric@gmail.com>
2023-04-29 00:24:09 -0700

    first commit

--- config/Allowedpage.js
+++ config/Allowedpage.js
@@ -1,4 +1,4 @@
-const AllowedSite = ["https://airbnb.com", "https://onrender.com/front-end", "http://localhost:2023", "http://127.0.0.1:3000"]
+const AllowedSite = ["https://airbnb.com", "https://airbnb-api-l62o.onrender.com"]
 
 const ops ={
     origin: (origin, callBack)=>{
