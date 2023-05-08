62938fb0b925916af69adcda4212c2f6bc11d61d
Ken Mikaelson <mikaelsoncedric@gmail.com>
2023-05-08 02:16:42 -0700

    first commit

--- config/Allowedpage.js
+++ config/Allowedpage.js
@@ -1,14 +1,14 @@
-const AllowedSite = ["https://airbnb.com", "https://airbnb-api-l62o.onrender.com", "https://airbnb-simple.onrender.com", "https://teck-mneg.onrender.com"]
+const AllowedSite = ["https://airbnb.com", "https://teck-mneg.onrender.com", "https://airbnb-api-l62o.onrender.com"];
 
-const ops ={
-    origin: (origin, callBack)=>{
-        if(AllowedSite.indexOf(origin) !== -1 || !origin ){
-            callBack(null, true)
-        }else{
-            callBack(new Error("You're not allowed to use our backend Api"))
-        }
-    },
-    optionsSuccessSatus: 200
-}
+const ops = {
+	origin: (origin, callBack) => {
+		if (AllowedSite.indexOf(origin) !== -1 || !origin) {
+			callBack(null, true);
+		} else {
+			callBack(new Error("You're not allowed to use our backend Api"));
+		}
+	},
+	optionsSuccessSatus: 200,
+};
 
 module.exports = ops;
